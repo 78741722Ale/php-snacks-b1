87d9aed7f5ae8d5d9260d9f2f894c3c719127c0d
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-20 14:53:25 +0200

    Snack 2 completo di cicli e soluzione finale

--- snack-2/index.php
+++ snack-2/index.php
@@ -18,28 +18,40 @@ $age = ($_GET['anni']); // la salvo in variabile
 /* In effetti a schermo esce tutto */
 var_dump("Io mi chiamo $nome". ' ' . "questa è la mia mail $mail" . ' ' . "e ho $age anni");
 
-/* Avvio i condizionali */
-/* name sia più lungo di 3 caratteri */
-/* mail contenga un punto e una chiocciola */
-/* age sia un numero */
+/* 
+Avvio i condizionali (la condizione finale è in fondo, 
+per esercitarmi coi condizionali provo a scrivere un po :) 
+*/
 
+/* name sia più lungo di 3 caratteri */
+if(strlen($nome) > 3) {
+    var_dump("$nome è composto da più di 3 caratteri");
+} else {
+    var_dump("$nome è composto da meno di 3 caratteri");
+};
+
+/* mail contenga un punto e una chiocciola */
+if(strpos($mail, '.') && strpos($mail, '@')) {
+    var_dump("$mail è corretta è ha i caratteri necessari");
+} else {
+    var_dump("$mail non è corretta, mancano caratteri fondamentali");
+};
+
+/* age sia un numero */
+if(is_numeric($age)) {
+    var_dump("Si, il numero è valido");
+} else {
+    var_dump("Non va bene, l'età dev'essere un numero! ");
+};
+
+/* Condizione finale, correzione esercizio */
 if(strlen($nome) > 3 && strpos($mail, '.') && strpos($mail, '@') && is_numeric($age)) {
-    var_dump("Accesso Consentito");
+    var_dump("Accesso Consentito"); // tutte e 3 le condizioni devono essere vere
+    echo("Accesso Consentito");
 } 
 else{
-    var_dump("Accesso Negato");
+    var_dump("Accesso Negato"); // altrimenti accesso negato
+    echo("Accesso Negato");
 }; 
-
-
-
-
-/* mail contenga un punto e una chiocciola */
-/* if(strpos($mail, '.') || strpos($mail, '@')) {
-    var_dump("Si, la mail contiene un punto e una chiocciola");
-} else {
-    var_dump("Accesso Negato");
-} */
-
-
 
 ?>
