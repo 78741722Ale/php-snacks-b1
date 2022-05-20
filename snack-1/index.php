f6a3610f199d2b2cf58e66c5dfa898e22737752b
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-20 12:42:42 +0200

    Initial Commit

---
+++ snack-1/index.php
@@ -0,0 +1,41 @@
+<?php 
+
+/*  
+## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa 
+e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+*/
+
+/* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario */
+$partite = 
+[
+    [
+        /* Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla 
+        squadra di casa e punti fatti dalla squadra ospite */
+        "Casa" => "Milano",
+        "Punteggio-casa" => 30,
+        "Ospiti" => "Cantu",
+        "Punteggio-Ospiti" => 35,
+    ],
+    [
+        "Casa" => "Bergamo",
+        "Punteggio-casa" => 33,
+        "Ospiti" => "Brescia",
+        "Punteggio-Ospiti" => 39,
+    ],
+    [
+        "Casa" => "Cosenza",
+        "Punteggio-casa" => 47,
+        "Ospiti" => "Palermo",
+        "Punteggio-Ospiti" => 55,
+    ],    
+];
+
+/* Verifico in dump */
+var_dump($partite);
+/* Prima partita */
+var_dump("$partite[0][Casa]")
+
+?>
