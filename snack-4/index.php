d550a66d8579ef3bbb8a48a37223f3cd01cea3cb
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-20 15:49:58 +0200

    Snack 4 completo
    Per il numero randomico aggiunto una funzione specifica che ritorna dei parametri. Provato con e senza function

--- snack-4/index.php
+++ snack-4/index.php
@@ -6,6 +6,33 @@ contenere lo stesso numero più di una volta */
 /* Creo un array di numeri vuoto */
 $numbers = [];
 var_dump("La lunghezza dell'array vuoto è : " . count($numbers)); // verifico in console log
+/* Genero un numero randomico */
+/* $rand_int = rand(1,150); => si genera ma rimane sempre costante */
+var_dump("Questo è il numero random : " . $rand_int);
 
+/* Avvio una funzione per un random integer */
+/** 
+ * getRandomInteger => numero randomico da 1 a x numeri
+ * $min => numero minimo
+ * $max => numero massimo
+ * 
+ * return => ritorna un numero randomico
+ */
+function getRandomInteger($min, $max) {
+    return rand($min,$max);
+}
 
+/* Avvio il ciclo for */
+for($i = 0 ; $i <= 15 ; $i++) {
+    $rand_int = getRandomInteger(1, 200); // Con una funzione
+    /* $rand_int = rand(1, 200) => senza funzione */
+    if(in_array($rand_int, $numbers)) {
+        var_dump("Il numero" . ' ' . $rand_int . ' ' . "è già presente e non può essere pushato");
+    } else {
+        array_push($numbers, $rand_int); // altrimento pusho il numero !
+    }
+}
+
+/* Verifico nuovamente in console il mio array inizialmente vuoto */
+var_dump($numbers)
 ?>
