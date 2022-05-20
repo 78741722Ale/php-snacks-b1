1cbbeb7db9a14259d7bcdc3e4ac530578db7f241
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-20 15:22:08 +0200

    Iniziato con snack 4 (fermo 10 min per errore imprevisto)

---
+++ snack-4/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+/* Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà 
+contenere lo stesso numero più di una volta */
+
+/* Creo un array di numeri vuoto */
+$numbers = [];
+var_dump("La lunghezza dell'array vuoto è : " . count($numbers)); // verifico in console log
+
+
+?>
